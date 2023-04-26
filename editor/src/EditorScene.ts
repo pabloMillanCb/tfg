40973268623aee75414b6c758d4cd5eba0d90e13
Pablo Millan <pablomillancb@gmail.com>
2023-04-26 11:57:22 +0200

    Creacion y destruccion de objetios, transformaciones, movimientos del raton, control de camara

--- editor/src/EditorScene.ts
+++ editor/src/EditorScene.ts
@@ -1,28 +1,99 @@
 import * as THREE from 'three'
+import { Box3, Object3D } from 'three';
+
 
 export default class EditorScene extends THREE.Scene
 {
-    private cube?: THREE.Mesh
-    private objectList: THREE.Mesh[] = []
+
+    // Lista de objetos en la escena
+    private liveObjects: THREE.Group = new THREE.Group
+    private selectedObject: THREE.Object3D = new THREE.Object3D
 
     initialize()
     {
-        const geometry = new THREE.BoxGeometry()
-        const material = new THREE.MeshPhongMaterial({color: 0xFFAD00})
+        var cube: THREE.Object3D = new THREE.Object3D() 
+        const edges = new THREE.EdgesGeometry( new THREE.BoxGeometry() );
+        cube.add(new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshPhongMaterial({color: 0xFFAD00})))
+        cube.position.y = 0.5
+        cube.position.x = -3
+        cube.position.z = -1
 
-        this.cube = new THREE.Mesh(geometry, material)
-        this.cube.position.z = -5
-        this.cube.position.y = -1
+        var sphere: THREE.Object3D = new THREE.Object3D() 
+        sphere.add(new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({color: 0xFFAD00})))
+        sphere.position.y = 0.5
+        sphere.position.x = 1
+        sphere.position.z = 3
 
-        this.add(this.cube)
+        this.addObjetct(cube)
+        this.addObjetct(sphere)
 
-        const light = new THREE.DirectionalLight(0xFFFFFF, 1)
-        light.position.set(0, 4, 2)
-        this.add(light)
+        this.add(this.liveObjects)
+
+        this.createBasics()
     }
 
     update()
     {
-        this.cube?.rotateY(0.05)
+        console.log("live: "+this.liveObjects.children.length)
+        console.log("scene: "+this.children.length)
     }
+
+    createBasics()
+    {
+        // Luz
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+        light.position.set(0, 4, 2)
+        this.add(light)
+
+        // Suelo de referencia
+        const gridHelper = new THREE.GridHelper( 10, 10 );
+        gridHelper.position.z = 0.1
+        this.add( gridHelper );
+
+        // Ejes de referencia
+        const axesHelper = new THREE.AxesHelper( 2 );
+        this.add( axesHelper );
+    }
+
+    addObjetct(obj: THREE.Object3D)
+    {
+        obj.name = "alive"
+        this.liveObjects.add(obj)
+    }
+
+    removeObject(obj: THREE.Object3D)
+    {
+        while (obj.name != "alive"){
+
+            obj = obj.parent!
+        }
+        
+        obj.removeFromParent()
+    }
+
+    getObjectArray():THREE.Object3D[]
+    {
+        return this.liveObjects.children
+    }
+
+    getSelectGroup(): THREE.Object3D
+    {
+        return this.selectedObject
+    }
+
+    selectObject(obj: THREE.Object3D)
+    {
+        while (obj.name != "alive"){
+
+            obj = obj.parent!
+        }
+
+        this.selectedObject = obj
+    }
+
+    unSelectAll()
+    {
+        this.selectedObject = new THREE.Object3D
+    }
+
 }
--- editor/src/main.ts
+++ editor/src/main.ts
@@ -1,27 +1,135 @@
 import * as THREE from 'three'
 import EditorScene from './EditorScene'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls'
 
 const width = window.innerWidth
 const height = window.innerHeight
 
-const renderer = new THREE.WebGLRenderer({
+// Creación del renderer
+var renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('app') as HTMLCanvasElement
 })
 renderer.setSize(width, height)
 renderer.setClearColor(new THREE.Color( 0x333333), 1)
 
-const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
+// Creación de la cámara
+const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 500)
+camera.position.set( 7, 10, 10 );
 
+// Creación de la escena
 const scene = new EditorScene()
 scene.initialize()
 
+// Creación de los controles de la cámara
+const cameraControl = new OrbitControls (camera, renderer.domElement)
+cameraControl.target = new THREE.Vector3(0, 0, 0)
+cameraControl.mouseButtons = {
+	MIDDLE: THREE.MOUSE.PAN,
+	RIGHT: THREE.MOUSE.ROTATE
+}
+cameraControl.zoomSpeed = 2
+cameraControl.update()
 
+// Creación de los controles de transformación
+const transformControls = new TransformControls(camera, renderer.domElement)
+transformControls.setMode("translate")
+scene.add(transformControls)
 
-function tick() 
+// Raycaster para selección con el ratón
+const raycaster: THREE.Raycaster = new THREE.Raycaster
+const raycasterHelper: THREE.Raycaster = new THREE.Raycaster
+const mouse: THREE.Vector2 = new THREE.Vector2(1, 1)
+
+// Definicion de listeners
+document.addEventListener( 'mousemove', onMouseMove );
+window.addEventListener( 'resize', onWindowResize );
+window.addEventListener( 'mousedown', onMouseDown );
+window.addEventListener( 'keydown', onKeyDown );
+window.addEventListener( 'keyup', onKeyUp );
+
+let key_pressed: { [key: string]: boolean } = {}
+
+// Función para refrescar cada frame
+function update() 
 {
   scene.update()
-  renderer.render(scene, mainCamera)
-  requestAnimationFrame(tick)
+
+  cameraControl.update()
+
+
+  requestAnimationFrame(update)
+
+  renderer.render(scene, camera)
 }
 
-tick()
+// Funciones para la interacción por teclado y ratón
+
+function onMouseMove( event : any )
+{
+  event.preventDefault();
+
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+function onWindowResize()
+{
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onMouseDown( e : any )
+{
+  if (e.button == 0) {
+    raycaster.setFromCamera( mouse, camera );
+    const intersection = raycaster.intersectObjects( scene.getObjectArray(), true)
+    
+     if (intersection.length > 0 && !transformControls.dragging) {
+
+      scene.selectObject(intersection[0].object)
+      transformControls.attach(scene.getSelectGroup())
+      
+      if (key_pressed['d']){
+        transformControls.detach()
+        scene.removeObject(intersection[0].object)
+      }
+    }
+    
+    else if (!transformControls.dragging){
+      transformControls.detach()
+      scene.unSelectAll()
+    }
+  }
+  
+}
+
+function onKeyDown(ev : any)
+{
+  key_pressed[ev.key.toLowerCase()] = true;
+
+  if (key_pressed['i']) {
+    transformControls.setMode("translate")
+  }
+
+  if (key_pressed['o']) {
+    transformControls.setMode("rotate")
+  }
+
+  if (key_pressed['p']) {
+    transformControls.setMode("scale")
+  }
+}
+
+function onKeyUp(ev : any)
+{
+  key_pressed[ev.key.toLowerCase()] = false;
+
+}
+
+update()
