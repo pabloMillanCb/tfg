472914ddf15a17d967f3b71f0e1b810362341f90
Pablo Millan <pablomillancb@gmail.com>
2023-06-14 20:08:05 +0200

    Cambiada ruta de node para archivos secretos

--- backend/util/admin.js
+++ backend/util/admin.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin");
 
 
-var serviceAccount = require("../etc/secrets/tfg-backend-2fd4c-firebase-adminsdk-tmyuf-b890a8439b.json");
+var serviceAccount = require("/etc/secrets/tfg-backend-2fd4c-firebase-adminsdk-tmyuf-b890a8439b.json");
 
 admin.initializeApp({
 
