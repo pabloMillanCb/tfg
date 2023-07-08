975b67d2d2e15fcf6adaa7e6224a939f04cb9fa7
pabloMillanCb <pablomillancb@gmail.com>
2023-07-08 12:43:33 +0200

    Archivo de configuración por entorno

--- editor/src/config/firebase-config.ts
+++ editor/src/config/firebase-config.ts
@@ -1,26 +1,12 @@
 import { initializeApp } from "firebase/app";
-
+import firebaseConfig from "/etc/secrets/firebase-config.json"
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 
 // Your web app's Firebase configuration
 
-const firebaseConfig = {
 
-    apiKey: "AIzaSyAaTOOaGBqUfk9oZQUc18nWpdSudiLa_nY",
-  
-    authDomain: "tfg-backend-2fd4c.firebaseapp.com",
-  
-    projectId: "tfg-backend-2fd4c",
-  
-    storageBucket: "tfg-backend-2fd4c.appspot.com",
-  
-    messagingSenderId: "628724485334",
-  
-    appId: "1:628724485334:web:029801f94181617d750d77"
-  
-  };
   
 const firebase = initializeApp(firebaseConfig); //initialize firebase app 
 
